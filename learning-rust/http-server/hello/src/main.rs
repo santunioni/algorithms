22143fb85ee950fac357f6d89f315b908c34c341
Vinícius Vargas <vinicius.vargas@ifood.com.br>
2025-03-03 10:27:30 -0300

    return http hard coded response

--- learning-rust/http-server/hello/src/main.rs
+++ learning-rust/http-server/hello/src/main.rs
@@ -1,13 +1,29 @@
-use std::net::TcpListener;
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
         println!(
-            "Connection established. I should interpret the request and send a response to {:?}",
-            _stream.peer_addr().unwrap()
+            "Connection established. I should interpret the request and send a response to {}",
+            stream.peer_addr().unwrap().to_string()
         );
+        handle_connection(stream)
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
+
+    let response = "HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
 }
