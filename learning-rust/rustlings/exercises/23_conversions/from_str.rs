d71aef262867777136ed4e3ea7612cc15784f4af
Vinícius Vargas <vinicius.vargas@ifood.com.br>
2025-03-08 20:58:12 -0300

    rustlings: implement solutions to exercises

--- learning-rust/rustlings/exercises/23_conversions/from_str.rs
+++ learning-rust/rustlings/exercises/23_conversions/from_str.rs
@@ -41,7 +41,26 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut s_split = s.split(',');
+
+        let (Some(name), Some(age), None) = (
+            s_split.next().map(|v| v.to_string()),
+            s_split.next().map(|v| v.parse::<u8>()),
+            s_split.next(),
+        ) else {
+            return Err(ParsePersonError::BadLen);
+        };
+
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        match age {
+            Ok(age) => Ok(Person { name, age }),
+            Err(err) => Err(ParsePersonError::ParseInt(err)),
+        }
+    }
 }
 
 fn main() {
