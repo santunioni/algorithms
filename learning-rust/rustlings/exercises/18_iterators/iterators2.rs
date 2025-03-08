c78f95416a2a0377eef86119dba12709ab18b990
Vinícius Vargas <vinicius.vargas@ifood.com.br>
2025-03-08 18:27:04 -0300

    rustlings: implement solutions to exercises

--- learning-rust/rustlings/exercises/18_iterators/iterators2.rs
+++ learning-rust/rustlings/exercises/18_iterators/iterators2.rs
@@ -7,7 +7,16 @@ fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => {
+            // let mut mystr = String::with_capacity(input.len());
+            // first.to_uppercase().for_each(|v| {mystr.push(v);});
+            // for following in chars {
+            //     mystr.push(following);
+            // }
+            // mystr
+
+            first.to_uppercase().to_string() + chars.as_str()
+        },
     }
 }
 
@@ -15,14 +24,14 @@ fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    // ???
+    words.iter().map(|v| capitalize_first(v)).collect::<Vec<String>>()
 }
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
 // slices. Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 fn capitalize_words_string(words: &[&str]) -> String {
-    // ???
+    words.iter().map(|v| capitalize_first(v)).collect::<String>()
 }
 
 fn main() {
