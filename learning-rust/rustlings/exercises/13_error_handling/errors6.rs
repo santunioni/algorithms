fd37583a668f7ba317ade0071b01d2249290a53d
Vinícius Vargas <vinicius.vargas@ifood.com.br>
2025-03-08 17:50:48 -0300

    rustlings: implement solutions to exercises

--- learning-rust/rustlings/exercises/13_error_handling/errors6.rs
+++ learning-rust/rustlings/exercises/13_error_handling/errors6.rs
@@ -19,6 +19,12 @@ enum ParsePosNonzeroError {
     ParseInt(ParseIntError),
 }
 
+impl From<ParseIntError> for ParsePosNonzeroError {
+    fn from(parse_int_error: ParseIntError) -> Self {
+        ParsePosNonzeroError::ParseInt(parse_int_error)
+    }
+}
+
 impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> Self {
         Self::Creation(err)
@@ -43,7 +49,7 @@ impl PositiveNonzeroInteger {
     fn parse(s: &str) -> Result<Self, ParsePosNonzeroError> {
         // TODO: change this to return an appropriate error instead of panicking
         // when `parse()` returns an error.
-        let x: i64 = s.parse().unwrap();
+        let x: i64 = s.parse()?;
         Self::new(x).map_err(ParsePosNonzeroError::from_creation)
     }
 }
