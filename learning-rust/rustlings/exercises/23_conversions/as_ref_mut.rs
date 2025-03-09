99dcd671dfdc6978315cc060d97cfc6640841f3b
Vinícius Vargas <vinicius.vargas@ifood.com.br>
2025-03-08 21:42:45 -0300

    rustlings: implement solutions to exercises

--- learning-rust/rustlings/exercises/23_conversions/as_ref_mut.rs
+++ learning-rust/rustlings/exercises/23_conversions/as_ref_mut.rs
@@ -1,3 +1,4 @@
+use std::ops::Mul;
 // AsRef and AsMut allow for cheap reference-to-reference conversions. Read more
 // about them at https://doc.rust-lang.org/std/convert/trait.AsRef.html and
 // https://doc.rust-lang.org/std/convert/trait.AsMut.html, respectively.
@@ -5,20 +6,23 @@
 // Obtain the number of bytes (not characters) in the given argument
 // (`.len()` returns the number of bytes in a string).
 // TODO: Add the `AsRef` trait appropriately as a trait bound.
-fn byte_counter<T>(arg: T) -> usize {
-    arg.as_ref().len()
+fn byte_counter<T: AsRef<str>>(arg: T) -> usize {
+    let as_str_ref = arg.as_ref();
+    as_str_ref.len()
 }
 
 // Obtain the number of characters (not bytes) in the given argument.
 // TODO: Add the `AsRef` trait appropriately as a trait bound.
-fn char_counter<T>(arg: T) -> usize {
-    arg.as_ref().chars().count()
+fn char_counter<T: AsRef<str>>(arg: T) -> usize {
+    let as_str_ref = arg.as_ref();
+    as_str_ref.chars().count()
 }
 
 // Squares a number using `as_mut()`.
 // TODO: Add the appropriate trait bound.
-fn num_sq<T>(arg: &mut T) {
-    // TODO: Implement the function body.
+fn num_sq<T: AsMut<u32>>(arg: &mut T) {
+    let as_u32_mutable_ref = arg.as_mut();
+    *as_u32_mutable_ref = as_u32_mutable_ref.mul(*as_u32_mutable_ref)
 }
 
 fn main() {
