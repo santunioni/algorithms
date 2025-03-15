229e3936eed35e24b6f98b459274c912c638ccac
Vinícius Vargas <vinicius.vargas@ifood.com.br>
2025-03-15 20:46:32 -0300

    feat: refactor quicksort implementation to use split_at_pivot function

--- grokking/src/chapter_4_quicksort.rs
+++ grokking/src/chapter_4_quicksort.rs
@@ -1,28 +1,28 @@
+fn split_at_pivot<T>(vec_to_split: Vec<T>) -> (Vec<T>, (usize, T), Vec<T>)
+where
+    T: PartialOrd + Copy,
+{
+    let pivot_index = vec_to_split.len() / 2;
+    let pivot_value = vec_to_split[pivot_index];
+
+    let (lesser_than_pivot, greater_than_pivot) = vec_to_split
+        .into_iter()
+        .enumerate()
+        .filter(|&(i, _)| i != pivot_index)
+        .map(|(_, v)| v)
+        .partition(|&v| v < pivot_value);
+
+    (
+        lesser_than_pivot,
+        (pivot_index, pivot_value),
+        greater_than_pivot,
+    )
+}
+
 fn quick_sorted<T>(vec: Vec<T>) -> Vec<T>
 where
     T: PartialOrd + Copy,
 {
-    fn split_at_pivot<T>(vec_to_split: Vec<T>) -> (Vec<T>, (usize, T), Vec<T>)
-    where
-        T: PartialOrd + Copy,
-    {
-        let pivot_index = vec_to_split.len() / 2;
-        let pivot_value = vec_to_split[pivot_index];
-
-        let (lesser_than_pivot, greater_than_pivot) = vec_to_split
-            .into_iter()
-            .enumerate()
-            .filter(|&(i, _)| i != pivot_index)
-            .map(|(_, v)| v)
-            .partition(|&v| v < pivot_value);
-
-        (
-            lesser_than_pivot,
-            (pivot_index, pivot_value),
-            greater_than_pivot,
-        )
-    }
-
     if vec.len() < 2 {
         return vec;
     };
