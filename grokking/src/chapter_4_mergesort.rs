a5a1179bb5ab163558a8d3cdc1013b75a6ae0414
Vinícius Vargas <vinicius.vargas@ifood.com.br>
2025-03-17 10:14:54 -0300

    feat: implement recursive merge sort and split vector functionality

--- grokking/src/chapter_4_mergesort.rs
+++ grokking/src/chapter_4_mergesort.rs
@@ -37,8 +37,21 @@ fn merge_vectors<T: PartialOrd>(mut first_half: Vec<T>, mut second_half: Vec<T>)
     new_array
 }
 
-fn merged_sorted_vec<T: PartialOrd>(vec: &mut Vec<T>) {
-    todo!()
+fn split_vector<T>(mut vec: Vec<T>) -> (Vec<T>, Vec<T>) {
+    let first_half: Vec<T> = vec.drain(0..vec.len() / 2).collect();
+    let second_half: Vec<T> = vec.drain(0..).collect();
+    (first_half, second_half)
+}
+
+fn merged_sorted_vec<T: PartialOrd>(mut vec: Vec<T>) -> Vec<T> {
+    if vec.len() == 1 {
+        return vec;
+    }
+
+    let (mut first_half, mut second_half) = split_vector(vec);
+    first_half = merged_sorted_vec(first_half);
+    second_half = merged_sorted_vec(second_half);
+    merge_vectors(first_half, second_half)
 }
 
 #[cfg(test)]
@@ -47,22 +60,33 @@ mod tests {
 
     #[test]
     fn should_sort_array_in_place() {
-        let mut my_vec = vec![1, 51512, 7, 4, 23, 45, 7, 8];
-        merged_sorted_vec(&mut my_vec);
+        let my_vec = vec![1, 51512, 7, 4, 23, 45, 7, 8];
+        let my_vec = merged_sorted_vec(my_vec);
         assert_eq!(my_vec, vec![1, 4, 7, 7, 8, 23, 45, 51512])
     }
 
     #[test]
     fn should_sort_big_array_in_place() {
-        let mut my_vec = (0..3000000).rev().collect::<Vec<u32>>();
-        merged_sorted_vec(&mut my_vec);
+        let my_vec = (0..3000000).rev().collect::<Vec<u32>>();
+        let my_vec = merged_sorted_vec(my_vec);
         assert_eq!(my_vec, (0..3000000).collect::<Vec<u32>>())
     }
 
     #[test]
-    fn should_merge_sorted_vectors() {
+    fn should_merge_sorted_alternating_vectors() {
         let vec_odd = vec![1, 3, 5, 7, 9];
         let vec_even = vec![2, 4, 6, 8, 10];
+
+        assert_eq!(
+            merge_vectors(vec_odd, vec_even),
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        )
+    }
+
+    #[test]
+    fn should_merge_sorted_ordered_vectors() {
+        let vec_odd = vec![1, 2, 3, 4, 5];
+        let vec_even = vec![6, 7, 8, 9, 10];
 
         assert_eq!(
             merge_vectors(vec_odd, vec_even),
