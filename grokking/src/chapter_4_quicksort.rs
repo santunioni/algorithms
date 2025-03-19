e5f5b64712f11510a0934156ede419f8d76ae50b
Vinícius Vargas <vinicius.vargas@ifood.com.br>
2025-03-19 13:00:07 -0300

    feat: update partition_slice function to return array instead of tuple

--- grokking/src/chapter_4_quicksort.rs
+++ grokking/src/chapter_4_quicksort.rs
@@ -5,7 +5,7 @@
 /// # Returns
 /// A tuple of two arrays. It is guaranteed the all items in first array are lesser
 /// than all items in second array.
-fn partition_slice<T>(slice: &mut [T]) -> (&mut [T], &mut [T])
+fn partition_slice<T>(slice: &mut [T]) -> [&mut [T]; 2]
 where
     T: PartialOrd,
 {
@@ -15,45 +15,46 @@ where
         if length == 2 && slice[0] > slice[1] {
             slice.swap(0, 1)
         }
-        return (&mut [], &mut []);
+        return [&mut [], &mut []];
     }
 
     let pivot_idx = length / 2;
     let pivot_raw_ptr = &raw mut slice[pivot_idx];
 
-    let mut lower_idx = 0;
-    let mut higher_idx = length - 1;
+    let mut lower_index = 0;
+    let mut higher_index = length - 1;
 
-    while lower_idx < higher_idx {
-        while unsafe { slice[lower_idx] < *pivot_raw_ptr } || lower_idx == pivot_idx {
-            lower_idx += 1;
+    while lower_index < higher_index {
+        while unsafe { slice[lower_index] < *pivot_raw_ptr } || lower_index == pivot_idx {
+            lower_index += 1;
         }
 
-        while (unsafe { slice[higher_idx] > *pivot_raw_ptr } && higher_idx > 0)
-            || higher_idx == pivot_idx
+        while (unsafe { slice[higher_index] > *pivot_raw_ptr } && higher_index > 0)
+            || higher_index == pivot_idx
         {
-            higher_idx -= 1;
+            higher_index -= 1;
         }
 
-        if lower_idx >= higher_idx {
+        if lower_index >= higher_index {
             break;
         }
 
-        slice.swap(lower_idx, higher_idx)
+        slice.swap(lower_index, higher_index)
     }
 
-    let new_pivot_idx = if lower_idx < pivot_idx {
-        slice.swap(lower_idx, pivot_idx);
-        lower_idx
-    } else if higher_idx > pivot_idx {
-        slice.swap(higher_idx, pivot_idx);
-        higher_idx
+    let displaced_pivot_index = if lower_index < pivot_idx {
+        slice.swap(lower_index, pivot_idx);
+        lower_index
+    } else if higher_index > pivot_idx {
+        slice.swap(higher_index, pivot_idx);
+        higher_index
     } else {
         pivot_idx
     };
 
-    let (first_half, second_half) = slice.split_at_mut(new_pivot_idx);
-    (first_half, &mut second_half[1..])
+    let (first_half, second_half) = slice.split_at_mut(displaced_pivot_index);
+    let second_half_without_pivot = &mut second_half[1..];
+    [first_half, second_half_without_pivot]
 }
 
 fn filter_slices_yet_to_sort<T: PartialOrd>(slices_to_sort: Vec<&mut [T]>) -> Vec<&mut [T]> {
@@ -62,15 +63,13 @@ fn filter_slices_yet_to_sort<T: PartialOrd>(slices_to_sort: Vec<&mut [T]>) -> Ve
         // Could be easily parallelized for larger datasets, thanks to functional style.
         // But for smaller datasets, the overhead of context switching between threads
         // isn't compensated by parallelism in computation.
-        .flat_map(|slice| {
-            let (lesser_half, greater_half) = partition_slice(slice);
-            vec![lesser_half, greater_half]
-        })
+        .flat_map(partition_slice)
         // Only slices with 2 or more items need partitioning
         .filter(|slice| slice.len() >= 2)
         .collect()
 }
 
+/// A loop version of quicksort, the fastest possible
 fn quick_sorted_loop<T: PartialOrd>(mut slices_to_sort: Vec<&mut [T]>) {
     while slices_to_sort.len() > 0 {
         slices_to_sort = filter_slices_yet_to_sort(slices_to_sort);
@@ -119,7 +118,7 @@ mod tests {
     #[test]
     fn should_partition_removing_pivot() {
         let mut my_vec = vec![1, 51512, 7, 4, 23, 45, 7, 8];
-        let (first_half, second_half) = partition_slice(&mut my_vec);
+        let [first_half, second_half] = partition_slice(&mut my_vec);
 
         assert_eq!(first_half, [1, 8, 7, 4, 7]);
         assert_eq!(second_half, [45, 51512]);
@@ -128,7 +127,7 @@ mod tests {
     #[test]
     fn should_partition_slice_of_two_numbers() {
         let mut my_vec = vec![1, 2];
-        let (first_half, second_half) = partition_slice(&mut my_vec);
+        let [first_half, second_half] = partition_slice(&mut my_vec);
 
         assert_eq!(first_half, []);
         assert_eq!(second_half, []);
@@ -137,7 +136,7 @@ mod tests {
     #[test]
     fn should_partition_slice_of_three_numbers() {
         let mut my_vec = vec![2, 1, 3];
-        let (first_half, second_half) = partition_slice(&mut my_vec);
+        let [first_half, second_half] = partition_slice(&mut my_vec);
 
         assert_eq!(first_half, []);
         assert_eq!(second_half, [2, 3]);
