88c521af127b4d8bc25ae3c2a0c62dd3ed9acfd7
Vinícius Vargas <vinicius.vargas@ifood.com.br>
2025-03-22 23:29:59 -0300

    feat: update Cell and LinkedList to use strong and weak references for improved memory management

--- grokking/src/chapter_4_linked_list.rs
+++ grokking/src/chapter_4_linked_list.rs
@@ -1,16 +1,17 @@
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 struct Cell<T> {
     item: Option<T>,
-    prev: Option<CellRef<T>>,
-    next: Option<CellRef<T>>,
+    prev: Option<CellWeakRef<T>>,
+    next: Option<CellStrongRef<T>>,
 }
 
-type CellRef<T> = Rc<RefCell<Cell<T>>>;
+type CellStrongRef<T> = Rc<RefCell<Cell<T>>>;
+type CellWeakRef<T> = Weak<RefCell<Cell<T>>>;
 
-impl<T> Into<CellRef<T>> for Cell<T> {
-    fn into(self) -> CellRef<T> {
+impl<T> Into<CellStrongRef<T>> for Cell<T> {
+    fn into(self) -> CellStrongRef<T> {
         Rc::new(RefCell::new(self))
     }
 }
@@ -23,28 +24,39 @@ impl<T> Cell<T> {
             next: None,
         }
     }
-    fn pop(&mut self) -> (Option<CellRef<T>>, Option<T>, Option<CellRef<T>>) {
+
+    fn pop(&mut self) -> (Option<CellWeakRef<T>>, Option<T>, Option<CellStrongRef<T>>) {
         let Cell { item, prev, next } = self;
 
+        let prev = if let Some(prev) = prev.take() {
+            prev.upgrade()
+        } else {
+            None
+        };
+
         match (&prev, &next) {
-            (Some(prev), Some(next)) => {
-                prev.borrow_mut().next = Some(Rc::clone(next));
-                next.borrow_mut().prev = Some(Rc::clone(prev));
+            (Some(prev_up), Some(next)) => {
+                prev_up.borrow_mut().next = Some(Rc::clone(next));
+                next.borrow_mut().prev = Some(Rc::downgrade(prev_up));
             }
             (Some(prev), None) => prev.borrow_mut().next = None,
             (None, Some(next)) => next.borrow_mut().prev = None,
             (None, None) => {}
         }
 
-        (prev.take(), item.take(), next.take())
+        (
+            prev.map(|prev| Rc::downgrade(&prev)),
+            item.take(),
+            next.take(),
+        )
     }
 
-    fn append(self_ref: CellRef<T>, item: T) -> CellRef<T> {
+    fn append(self_ref: CellStrongRef<T>, item: T) -> CellStrongRef<T> {
         let mut self_ref_mut = self_ref.borrow_mut();
         let old_next = self_ref_mut.next.take();
         let new_cell = Cell {
             item: Some(item),
-            prev: Some(Rc::clone(&self_ref)),
+            prev: Some(Rc::downgrade(&self_ref)),
             next: old_next,
         }
         .into();
@@ -52,7 +64,7 @@ impl<T> Cell<T> {
         new_cell
     }
 
-    fn prepend(self_ref: CellRef<T>, item: T) -> CellRef<T> {
+    fn prepend(self_ref: CellStrongRef<T>, item: T) -> CellStrongRef<T> {
         let mut self_ref_mut = self_ref.borrow_mut();
         let old_prev = self_ref_mut.prev.take();
         let new_cell = Cell {
@@ -61,15 +73,15 @@ impl<T> Cell<T> {
             prev: old_prev,
         }
         .into();
-        self_ref_mut.prev = Some(Rc::clone(&new_cell));
+        self_ref_mut.prev = Some(Rc::downgrade(&new_cell));
         new_cell
     }
 }
 
 pub struct LinkedList<T> {
     len: u64,
-    first: Option<CellRef<T>>,
-    last: Option<CellRef<T>>,
+    first: Option<CellStrongRef<T>>,
+    last: Option<CellWeakRef<T>>,
 }
 
 pub struct Drain<T>(LinkedList<T>);
@@ -129,8 +141,9 @@ impl<T> LinkedList<T> {
         self.first = if let Some(first) = self.first.take() {
             Some(Cell::prepend(first, new_first))
         } else {
-            self.last = Some(Cell::new(new_first).into());
-            self.last.as_ref().map(Rc::clone)
+            let cell = Cell::new(new_first).into();
+            self.last = Some(Rc::downgrade(&cell));
+            Some(cell)
         };
         self.len += 1;
     }
@@ -144,18 +157,19 @@ impl<T> LinkedList<T> {
     }
 
     pub fn add_last(&mut self, new_last: T) {
-        self.last = if let Some(last) = self.last.take() {
-            Some(Cell::append(last, new_last))
+        self.last = if let Some(last) = self.last.take().and_then(|v| v.upgrade()) {
+            Some(Rc::downgrade(&Cell::append(last, new_last)))
         } else {
-            self.first = Some(Cell::new(new_last).into());
-            self.first.as_ref().map(Rc::clone)
+            let cell = Cell::new(new_last).into();
+            self.first = Some(Rc::clone(&cell));
+            Some(Rc::downgrade(&cell))
         };
         self.len += 1;
     }
 
     pub fn pop_last(&mut self) -> Option<T> {
         let last = self.last.take()?;
-        let (prev, item, _) = last.borrow_mut().pop();
+        let (prev, item, _) = last.upgrade()?.borrow_mut().pop();
         self.last = prev;
         self.len -= 1;
         item
