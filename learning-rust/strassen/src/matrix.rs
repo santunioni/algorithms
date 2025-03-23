2c3bbc9a97d3de72b6fcd9c9467bbea18aea89b4
Vinícius Vargas <vinicius.vargas@ifood.com.br>
2025-03-23 20:30:29 -0300

    feat: update Matrix and SubMatrix structs to use i64 for data storage and improve indexing methods

--- learning-rust/strassen/src/matrix.rs
+++ learning-rust/strassen/src/matrix.rs
@@ -4,41 +4,43 @@ use std::ops::{Add, Index, IndexMut, Mul, Sub};
 pub struct Matrix {
     rows: usize,
     cols: usize,
-    data: Vec<usize>,
+    data: Vec<i64>,
 }
 
 #[derive(Clone, Debug)]
 struct SubMatrix<'a> {
-    rows: usize,
-    cols: usize,
-    data: &'a [usize],
+    rows_window_from_parent: (usize, usize),
+    cols_window_from_parent: (usize, usize),
+    parent: &'a Matrix,
+}
+
+impl<'a> SubMatrix<'a> {
+    fn rows(&self) -> usize {
+        self.rows_window_from_parent.1 - self.rows_window_from_parent.0
+    }
+
+    fn cols(&self) -> usize {
+        self.cols_window_from_parent.1 - self.cols_window_from_parent.0
+    }
 }
 
 impl<'a> From<&'a SubMatrix<'a>> for Matrix {
     fn from(value: &'a SubMatrix<'a>) -> Self {
-        Matrix {
-            rows: value.rows,
-            cols: value.cols,
-            data: value.data.into(),
-        }
+        value.parent.clone()
     }
 }
 
 impl<'a> From<&'a Matrix> for SubMatrix<'a> {
     fn from(value: &'a Matrix) -> Self {
         SubMatrix {
-            rows: value.rows,
-            cols: value.cols,
-            data: &value.data,
+            rows_window_from_parent: (0, value.rows),
+            cols_window_from_parent: (0, value.cols),
+            parent: &value,
         }
     }
 }
 
 type MatrixIndex = (usize, usize);
-
-fn convert_matrix_index_to_vec_index(cols: usize, matrix_index: MatrixIndex) -> usize {
-    matrix_index.1 + matrix_index.0 * cols
-}
 
 impl Matrix {
     pub fn zeroes(rows: usize, cols: usize) -> Self {
@@ -59,26 +61,27 @@ impl Matrix {
 }
 
 impl<'a> Index<MatrixIndex> for SubMatrix<'a> {
-    type Output = usize;
+    type Output = i64;
 
     fn index(&self, index: MatrixIndex) -> &Self::Output {
-        let vec_index = convert_matrix_index_to_vec_index(self.cols, index);
-        &self.data[vec_index]
+        let row = index.0 + self.rows_window_from_parent.0;
+        let column = index.1 + self.cols_window_from_parent.0;
+        &self.parent[(row, column)]
     }
 }
 
 impl Index<MatrixIndex> for Matrix {
-    type Output = usize;
+    type Output = i64;
 
     fn index(&self, index: MatrixIndex) -> &Self::Output {
-        let vec_index = convert_matrix_index_to_vec_index(self.cols, index);
+        let vec_index = index.1 + index.0 * self.cols;
         &self.data[vec_index]
     }
 }
 
 impl IndexMut<MatrixIndex> for Matrix {
     fn index_mut(&mut self, index: MatrixIndex) -> &mut Self::Output {
-        let vec_index = convert_matrix_index_to_vec_index(self.cols, index);
+        let vec_index = index.1 + index.0 * self.cols;
         &mut self.data[vec_index]
     }
 }
@@ -89,12 +92,12 @@ impl<'a> Sub<Self> for &SubMatrix<'a> {
     type Output = MatrixOperationResult;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        if self.rows != rhs.rows || self.cols != rhs.cols {
+        if self.rows() != rhs.rows() || self.cols() != rhs.cols() {
             return Err("Matrices dimensions do not match");
         }
         let mut result = Matrix::from(self);
-        for i in 0..self.rows {
-            for j in 0..self.cols {
+        for i in 0..self.rows() {
+            for j in 0..self.cols() {
                 result[(i, j)] -= rhs[(i, j)];
             }
         }
@@ -106,12 +109,12 @@ impl<'a> Add<Self> for &SubMatrix<'a> {
     type Output = MatrixOperationResult;
 
     fn add(self, rhs: Self) -> Self::Output {
-        if self.rows != rhs.rows || self.cols != rhs.cols {
+        if self.rows() != rhs.rows() || self.cols() != rhs.cols() {
             return Err("Matrices dimensions do not match");
         }
         let mut result = Matrix::from(self);
-        for i in 0..self.rows {
-            for j in 0..self.cols {
+        for i in 0..self.rows() {
+            for j in 0..self.cols() {
                 result[(i, j)] += rhs[(i, j)];
             }
         }
@@ -120,41 +123,79 @@ impl<'a> Add<Self> for &SubMatrix<'a> {
 }
 
 impl<'a> SubMatrix<'a> {
-    fn multiply_baseline(&self, other: &SubMatrix) -> MatrixOperationResult {
-        if self.cols != other.rows {
+    fn multiply_baseline(&self, rhs: &SubMatrix) -> MatrixOperationResult {
+        if self.cols() != rhs.rows() {
             return Err("Matrices dimensions do not match for multiplication");
         }
 
-        let mut result = Matrix::zeroes(self.rows, other.cols);
-        for i in 0..self.rows {
-            for j in 0..other.cols {
-                for k in 0..self.cols {
-                    result[(i, j)] += self[(i, k)] * other[(k, j)];
+        let mut result = Matrix::zeroes(self.rows(), rhs.cols());
+        for i in 0..self.rows() {
+            for j in 0..rhs.cols() {
+                for k in 0..self.cols() {
+                    result[(i, j)] += self[(i, k)] * rhs[(k, j)];
                 }
             }
         }
 
         Ok(result)
     }
+
+    // fn divide_in_4_parts(&'a self, at_col: usize, at_row: usize) -> [SubMatrix<'a>; 4] {
+    //     //! Matrix split order is defined in the picture
+    //     //! https://www.interviewbit.com/blog/wp-content/uploads/2021/12/New-quadrants-768x482.png
+    //     [
+    //         SubMatrix {
+    //             cols: at_col,
+    //             rows: at_row,
+    //             data: &[],
+    //         },
+    //         SubMatrix {
+    //             cols: self.cols - at_col,
+    //             rows: at_row,
+    //             data: &[],
+    //         },
+    //         SubMatrix {
+    //             cols: at_col,
+    //             rows: self.rows - at_row,
+    //             data: &[],
+    //         },
+    //         SubMatrix {
+    //             cols: self.cols - at_col,
+    //             rows: self.rows - at_row,
+    //             data: &[],
+    //         },
+    //     ]
+    // }
+
+    // fn strassen_split_with(&self, rhs: &Self) -> [SubMatrix; 8] {
+    //     let a_cols = self.cols / 2;
+    //     let b_cols = self.cols - a_cols;
+    //     let a_rows = self.rows / 2;
+    //     let c_rows = self.rows - a_rows;
+    //
+    //     let e_cols = rhs.cols / 2;
+    //     let f_cols = rhs.cols - e_cols;
+    //     []
+    // }
 }
 
 impl<'a> Mul<Self> for &SubMatrix<'a> {
     type Output = MatrixOperationResult;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        if self.cols != rhs.rows {
+        if self.cols() != rhs.rows() {
             return Err("Matrices dimensions do not match for multiplication");
         }
 
-        match (self.rows, self.cols, rhs.cols) {
+        match (self.rows(), self.cols(), rhs.cols()) {
             (self_rows, self_cols, other_cols)
                 if self_rows <= 2 || self_cols <= 2 || other_cols <= 2 =>
             {
                 self.multiply_baseline(&rhs)
             }
             (_self_rows, _self_cols, _other_cols) => {
-                // //! Matrix names are defined in the picture
-                // //! https://www.interviewbit.com/blog/wp-content/uploads/2021/12/New-quadrants-768x482.png
+                // Matrix names are defined in the picture
+                // https://www.interviewbit.com/blog/wp-content/uploads/2021/12/New-quadrants-768x482.png
                 // let [a, b, c, d, e, f, g, h] = self.stranssen_split(rhs);
                 // // let p_1 =
                 //
