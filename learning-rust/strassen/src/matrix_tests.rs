87b35da5dec6caa6739b347e5cd05083c2a6d861
Vinícius Vargas <vinicius.vargas@ifood.com.br>
2025-03-23 17:28:07 -0300

    Changes by pedrodardengomesquita

---
+++ learning-rust/strassen/src/matrix_tests.rs
@@ -0,0 +1,46 @@
+use crate::matrix;
+use crate::matrix::Matrix;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+#[test]
+fn should_multiply_column_matrix_by_row_matrix() -> TestResult {
+    // ARRANGE
+    let row_matrix = matrix!([1, 2, 5]);
+    let column_matrix = matrix!([2], [3], [4]);
+
+    // ACT
+    let result = (&row_matrix * &column_matrix)?;
+
+    // ASSERT
+    assert_eq!(result, matrix![[28]]);
+    Ok(())
+}
+
+#[test]
+fn should_multiply_row_matrix_by_column_matrix() -> TestResult {
+    // ARRANGE
+    let column_matrix = matrix!([2], [3], [4]);
+    let row_matrix = matrix!([1, 2, 5]);
+
+    // ACT
+    let result = (&column_matrix * &row_matrix)?;
+
+    // ASSERT
+    assert_eq!(result, matrix![[2, 4, 10], [3, 6, 15], [4, 8, 20]]);
+    Ok(())
+}
+
+#[test]
+fn should_return_same_matrix_when_multiplying_by_identity() -> TestResult {
+    // ARRANGE
+    let matrix = matrix!([1, 2, 3], [4, 5, 6], [7, 8, 9]);
+    let identity = Matrix::identity(3);
+
+    // ACT
+    let result = (&matrix * &identity)?;
+
+    // ASSERT
+    assert_eq!(result, matrix);
+    Ok(())
+}
