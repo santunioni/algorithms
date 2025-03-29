5335f91f0275bcbe21bcee5fd378626a738ad87d
Vinícius Vargas <vinicius.vargas@ifood.com.br>
2025-03-29 01:44:51 -0300

    feat: implement recursive and loop binary search functions with updated return types and tests

--- grokking/src/chapter_4_recursive_binary_search.rs
+++ grokking/src/chapter_4_recursive_binary_search.rs
@@ -1,9 +1,9 @@
-fn binary_search<T: PartialOrd>(list: &[T], searched_value: &T) -> usize {
+fn recursive_binary_search<T: PartialOrd>(list: &[T], searched_value: &T) -> Option<usize> {
     fn tail_recursive_binary_search<T: PartialOrd>(
         list: &[T],
         searched_value: &T,
         start_index: usize,
-    ) -> usize {
+    ) -> Option<usize> {
         let mid_index = list.len() / 2;
         let mid_value: &T = &list[mid_index];
 
@@ -12,9 +12,12 @@ fn binary_search<T: PartialOrd>(list: &[T], searched_value: &T) -> usize {
             if searched_value < mid_value {
                 (first_half, start_index)
             } else if searched_value > mid_value {
+                if mid_index == 0 {
+                    return None;
+                }
                 (second_half, start_index + mid_index)
             } else {
-                return start_index + mid_index;
+                return Some(start_index + mid_index);
             }
         };
 
@@ -24,13 +27,60 @@ fn binary_search<T: PartialOrd>(list: &[T], searched_value: &T) -> usize {
     tail_recursive_binary_search(list, searched_value, 0)
 }
 
+fn loop_binary_search<T: PartialOrd>(list: &[T], searched_value: &T) -> Option<usize> {
+    let mut start_index = 0;
+    let mut list = list;
+
+    loop {
+        let mid_index = list.len() / 2;
+        let mid_value: &T = &list[mid_index];
+
+        let (first_half, second_half) = list.split_at(mid_index);
+        if searched_value < mid_value {
+            list = first_half;
+        } else if searched_value > mid_value {
+            if mid_index == 0 {
+                return None;
+            }
+            list = second_half;
+            start_index += mid_index;
+        } else {
+            return Some(start_index + mid_index);
+        }
+
+        if list.is_empty() {
+            break;
+        }
+    }
+
+    None
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::chapter_4_recursive_binary_search::binary_search;
+    use crate::chapter_4_recursive_binary_search::{loop_binary_search, recursive_binary_search};
 
     #[test]
-    fn should_return_found_value() {
+    fn should_return_found_value_recursive() {
         let my_ordered_array: Vec<u32> = (0..10u32.pow(6)).collect();
-        assert_eq!(binary_search(&my_ordered_array, &1037u32), 1037usize);
+        assert_eq!(recursive_binary_search(&my_ordered_array, &1037u32), Some(1037usize));
+    }
+
+    #[test]
+    fn should_not_return_found_value_recursive() {
+        let my_ordered_array = vec![0, 1, 2, 3, 5, 6, 7, 8, 9];
+        assert_eq!(recursive_binary_search(&my_ordered_array, &4), None);
+    }
+
+    #[test]
+    fn should_return_found_value_loop() {
+        let my_ordered_array: Vec<u32> = (0..10u32.pow(6)).collect();
+        assert_eq!(loop_binary_search(&my_ordered_array, &1037u32), Some(1037usize));
+    }
+
+    #[test]
+    fn should_not_return_found_value_loop() {
+        let my_ordered_array = vec![0, 1, 2, 3, 5, 6, 7, 8, 9];
+        assert_eq!(loop_binary_search(&my_ordered_array, &4), None);
     }
 }
